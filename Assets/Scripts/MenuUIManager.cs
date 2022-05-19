d995ba003f9f98eec0659771923f44ea878c2b2b
ebgames <84336806+ebgames@users.noreply.github.com>
2022-05-19 17:51:57 +0100

    Set up start button

---
+++ Assets/Scripts/MenuUIManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUIManager : MonoBehaviour
+{
+    // THIS IS MY PROJECT NOT THE EXAMPLE ONE
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+   
+}
